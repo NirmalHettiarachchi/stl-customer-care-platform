7a465e2e972912ec19b5d284eb02c5764fa1f57d
NirmalHettiarachchi <nirmalhettiarachchi5@gmail.com>
2024-08-30 19:01:13 +0530

    ESB route for sending notifications

---
+++ integration-esb/src/test/java/com/stl/integration_esb/IntegrationEsbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stl.integration_esb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationEsbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

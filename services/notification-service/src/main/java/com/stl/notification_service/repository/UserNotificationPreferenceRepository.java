2de8397d9eb24469c20a3dfa34a7cf0acc735a98
LNTR <101091694+LNTR@users.noreply.github.com>
2024-09-22 17:21:19 +0530

    clean up commit

--- services/notification-service/src/main/java/com/stl/notification_service/repository/UserNotificationPreferenceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.stl.notification_service.repository;
-
-import com.stl.notification_service.model.UserNotificationPreference;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserNotificationPreferenceRepository extends JpaRepository<UserNotificationPreference, String> {
-    UserNotificationPreference findByUsername(String username);
-}

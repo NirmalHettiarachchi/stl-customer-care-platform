94be227b32390f604427f06a065e96218563402e
NirmalHettiarachchi <nirmalhettiarachchi5@gmail.com>
2024-09-28 14:22:25 +0530

    Notification endpoints

--- services/notification-service/src/main/java/com/stl/notification_service/controller/KafkaController.java
+++ services/notification-service/src/main/java/com/stl/notification_service/controller/KafkaController.java
@@ -1,10 +1,13 @@
 package com.stl.notification_service.controller;
 
+import com.stl.notification_service.model.Notification;
 import com.stl.notification_service.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/notifications")
@@ -30,6 +33,12 @@ public class KafkaController {
         String username = getCurrentUsername();
         notificationService.updatePreferences(username, email, sms, push);
         return "Preferences updated successfully!";
+    }
+
+    @GetMapping
+    public List<Notification> getUserNotifications() {
+        String username = getCurrentUsername();
+        return notificationService.getNotificationsForUser(username);
     }
 
     private String getCurrentUsername() {
